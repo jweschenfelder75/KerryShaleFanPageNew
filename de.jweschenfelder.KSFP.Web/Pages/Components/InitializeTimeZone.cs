0cf5c8c8ad054bc70ad7bb263a2939ce691148ae
byte2702 <weschenfelder@gmx.de>
2024-08-05 07:27:05 +0100

    LocalTimeProvider added.

---
+++ de.jweschenfelder.KSFP.Web/Pages/Components/InitializeTimeZone.cs
@@ -0,0 +1,28 @@
+﻿using de.jweschenfelder.KSFP.Web.Providers;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace de.jweschenfelder.KSFP.Web.Pages.Components
+{
+    public class InitializeTimeZone : ComponentBase
+    {
+        [Inject] public TimeProvider TimeProvider { get; set; } = default!;
+        [Inject] public IJSRuntime JSRuntime { get; set; } = default!;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender && TimeProvider is BrowserTimeProvider browserTimeProvider && !browserTimeProvider.IsLocalTimeZoneSet)
+            {
+                try
+                {
+                    await using var module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./js/timezone.js");
+                    var timeZone = await module.InvokeAsync<string>("getBrowserTimeZone");
+                    browserTimeProvider.SetBrowserTimeZone(timeZone);
+                }
+                catch (JSDisconnectedException)
+                {
+                }
+            }
+        }
+    }
+}
---
+++ de.jweschenfelder.KSFP.Web/Pages/Components/LocalTime.cs
@@ -0,0 +1,45 @@
+﻿using de.jweschenfelder.KSFP.Web.Extensions;
+using de.jweschenfelder.KSFP.Web.Providers;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components;
+
+namespace de.jweschenfelder.KSFP.Web.Pages.Components
+{
+    public class LocalTime : ComponentBase, IDisposable
+    {
+        [Inject]
+        public TimeProvider TimeProvider { get; set; } = default!;
+
+        [Parameter]
+        public DateTime? DateTime { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (TimeProvider is BrowserTimeProvider browserTimeProvider)
+            {
+                browserTimeProvider.LocalTimeZoneChanged += LocalTimeZoneChanged;
+            }
+        }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            if (DateTime != null)
+            {
+                builder.AddContent(0, TimeProvider.ToLocalDateTime(DateTime.Value));
+            }
+        }
+
+        public void Dispose()
+        {
+            if (TimeProvider is BrowserTimeProvider browserTimeProvider)
+            {
+                browserTimeProvider.LocalTimeZoneChanged -= LocalTimeZoneChanged;
+            }
+        }
+
+        private void LocalTimeZoneChanged(object? sender, EventArgs e)
+        {
+            _ = InvokeAsync(StateHasChanged);
+        }
+    }
+}
