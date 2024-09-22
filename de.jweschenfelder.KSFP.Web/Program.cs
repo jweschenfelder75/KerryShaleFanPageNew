a554346a57ece3c414d34357f99e28a35d36c7a5
byte2702 <weschenfelder@gmx.de>
2024-09-22 10:41:17 +0200

    GitHook removed.

--- de.jweschenfelder.KSFP.Web/Program.cs
+++ de.jweschenfelder.KSFP.Web/Program.cs
@@ -15,8 +15,6 @@ using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 
-// $Id$ 
-
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("newssettings.json", optional: true, reloadOnChange: true);
 builder.Configuration.AddJsonFile("gallerysettings.json", optional: true, reloadOnChange: true);
